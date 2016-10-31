fe67680cfb4e4ad9af88e19abeb9284ad7a3a449
Tom W. Yedwab <tom@khanacademy.org>
2016-10-30 22:23:03 -0700

    Integrated SVM model into Chrome extension

--- extension/src/background.js
+++ extension/src/background.js
@@ -4,7 +4,9 @@
 
     function updateBadge(tabId) {
         var blocked = tabScripts[tabId] || {};
-        var numBlocked = Object.keys(blocked).filter(function(item) { return blocked[item] == 1; }).length;
+        var numBlocked = Object.keys(blocked).filter(
+            (item) => blocked[item].contentBlocked == 1 || blocked[item].urlBlocked == 1
+        ).length;
         chrome.browserAction.setBadgeBackgroundColor({color: "#ba3500"});
         if (numBlocked > 0) {
             chrome.browserAction.setBadgeText({text: "" + numBlocked});
@@ -13,36 +15,104 @@
         }
     }
 
-    function shouldBlock(url, contents) {
-        // TODO(tom): Insert model here!
-        console.log("shouldBlock", url, contents.substr(0, 50));
-        return {
-            url: 0,
-            content: 1
-        };
+    function tokenizeContents(contents) {
+        contents = contents
+            .replace(/(\/\*[^*]+\*\/)/, "")
+            .replace(/\/\/.+/, "")
+            .match(/[A-Z][a-z]+|[A-Z]+|[a-z]+|[0-9]+|[\-\\\/_{}\"\',\(\)\.:]|[\+\*=]|\*.+\*\//g)
+            .map((x) => (x.length === 1 && x >= "a" && x <= "z") ? "x" : x.toLowerCase());
+        return contents;
+    }
+
+    function calcSVMScore(tokens, model, max_ngram) {
+        // Build the SVM input vector
+        var inputVector = Array(model.w.length).fill(0.0);
+
+        // Check all n-grams for n=1..max_ngram
+        for (var n = 1; n <= max_ngram; n++) {
+            for (var p = 0; p < tokens.length + 1 - n; p++) {
+                var subTokens = tokens.slice(p, p + n).join(" ");
+                var index = model.vocab[subTokens];
+                if (index !== undefined) {
+                    inputVector[index] += model.idf[index];
+                }
+            }
+        }
+
+        // Normalize input vector
+        var mag = Math.sqrt(inputVector.reduce((p, n) => p + n*n, 0))
+        inputVector = inputVector.map(x => x / mag);
+
+        return inputVector.reduce((p, n, i) => p + n * model.w[i], 0);
+    }
+
+    function shouldBlockUrl(url, prevInfo) {
+        var urlModel = getUrlModel();
+
+        var urlTokens = url.toLowerCase().split("");
+        var urlScore = calcSVMScore(urlTokens, urlModel, 6);
+
+        // SVM score
+        var svmScore = urlModel.b + urlScore;
+
+        prevInfo.urlBlocked = (svmScore > 0) ? 1 : 0;
+        prevInfo.urlScore = svmScore;
+        return prevInfo;
+    }
+
+    function shouldBlockContents(url, contents, prevInfo) {
+        var combinedModel = getCombinedModel();
+
+        var urlTokens = url.toLowerCase().split("");
+        var urlScore = calcSVMScore(urlTokens, combinedModel.url, 6);
+
+        var scriptTokens = tokenizeContents(contents);
+        var scriptScore = calcSVMScore(scriptTokens, combinedModel.script, 2);
+
+        // SVM score
+        var svmScore = combinedModel.b + urlScore + scriptScore;
+
+        prevInfo.contentBlocked = (svmScore > 0) ? 1 : 0;
+        prevInfo.contentScore = svmScore;
+        return prevInfo;
     }
 
     chrome.webRequest.onBeforeRequest.addListener(function(details) {
         if (details.type !== "script" || details.method !== "GET") {
             return;
         }
+
+        tabScripts[details.tabId] = tabScripts[details.tabId] || {};
+
+        var result = {};
+
+        result = shouldBlockUrl(details.url, result);
+        if (result.urlBlocked === 1) {
+            console.log("## URL BLOCKED", details.url, result.urlScore);
+            tabScripts[details.tabId][details.url] = result;
+            updateBadge(details.tabId);
+            return {cancel: true};
+        }
+
         var request = new XMLHttpRequest();
         request.open('GET', details.url, false);  // `false` makes the request synchronous
         request.send(null);
 
         if (request.status !== 200) {
+            // TODO(tom): What to do here?
             return;
         }
 
-        var result = shouldBlock(details.url, request.responseText);
-
-        tabScripts[details.tabId] = tabScripts[details.tabId] || {};
+        result = shouldBlockContents(details.url, request.responseText, result);
         tabScripts[details.tabId][details.url] = result;
 
-        updateBadge(details.tabId);
-        if (result.content === 1) {
+        if (result.contentBlocked === 1) {
+            console.log("## CONTENT BLOCKED", details.url, result.contentScore);
+            updateBadge(details.tabId);
             return {cancel: true};
         }
+
+        console.log("Content passed", details.url, result.urlScore, result.contentScore)
         return;
     }, {urls: ["http://*/*", "https://*/*"], types: ["script"]}, ["blocking"]);
 
